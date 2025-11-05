5b8a3bd944c0f0ec4b0e0b7eb6614aa506715df0
Elshin Vadim <elshinvadim@gmail.com>
2025-11-05 13:55:58 +0100

    Starting ej 3

---
+++ JAVASCRIPT/UD2/Practica 6/Actividad 1/script.js
@@ -0,0 +1,74 @@
+
+function comprobarDni(campo) {
+    var re = /^[XYZ]?\d{5,8}-?[A-Z]$/;
+    if (re.test(campo.value)) {
+        document.getElementById("p_dni").textContent = campo.value;
+        document.getElementById(campo.name).className = "correcto";
+    } else {
+        campo.value = "";
+        document.getElementById(campo.name).className = "incorrecto";
+    }
+}
+
+function comprobarNombre(campo) {
+    var re = /([a-zA-Záéíóúñ]{4,40}( [a-zA-Záéíóúñ]{4,40})){1,2}/;
+    if (re.test(campo.value)) {
+        document.getElementById("p_nombre").textContent = campo.value;
+        document.getElementById(campo.name).className = "correcto";
+    } else {
+        campo.value = "";
+        document.getElementById(campo.name).className = "incorrecto";
+    }
+}
+
+function comprobarFecha(campo) {
+    if (campo.value != "") {
+        document.getElementById("p_fecha_nac").textContent = campo.value;
+        document.getElementById(campo.name).className = "correcto";
+    } else {
+        campo.value = "";
+        document.getElementById(campo.name).className = "incorrecto";
+    }
+}
+
+function comprobarNum(campo) {
+    if (campo.value >= 0 && campo.value < 11) {
+        document.getElementById("p_num_hijos").textContent = campo.value;
+        document.getElementById(campo.name).className = "correcto";
+    } else {
+        campo.value = "";
+        document.getElementById(campo.name).className = "incorrecto";
+    }
+}
+
+function comprobarEmail(campo) {
+    var re = /^[a-z]*[@]{1}[a-z]*[\.]{1}[a-z]{2,3}$/;
+    if (re.test(campo.value)) {
+        document.getElementById("p_email").textContent = campo.value;
+        document.getElementById(campo.name).className = "correcto";
+    } else {
+        campo.value = "";
+        document.getElementById(campo.name).className = "incorrecto";
+    }
+}
+
+function comprobarWeb(campo) {
+    var re = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/;
+    if (re.test(campo.value)) {
+        document.getElementById("p_pag_web").textContent = campo.value;
+        document.getElementById(campo.name).className = "correcto";
+    } else {
+        campo.value = "";
+        document.getElementById(campo.name).className = "incorrecto";
+    }
+}
+
+function comprobarContrasena(campo) {
+    if (campo.value.length > 7 && campo.value.length < 11) {
+        document.getElementById("p_contrasena").textContent = campo.value;
+        document.getElementById(campo.name).className = "correcto";
+    } else {
+        campo.value = "";
+        document.getElementById(campo.name).className = "incorrecto";
+    }
+}
