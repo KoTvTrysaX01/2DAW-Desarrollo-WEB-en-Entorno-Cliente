1f0796fe554aded4c58804aff778aa3f091f78cb
Elshin Vadim <elshinvadim@gmail.com>
2025-12-04 12:59:09 +0100

    Working on 8.1

--- JAVASCRIPT/UD3/Practica_8.1/login.js
+++ JAVASCRIPT/UD3/Practica_8.1/login.js
@@ -1,5 +1,5 @@
 function checkInput(field) {
-    if (field.value.length < 4) {
+    if (field.value.length < 3) {
         field.classList = "incorrect";
         return false;
     } else {
@@ -11,32 +11,27 @@ function checkInput(field) {
 
 document.getElementById("submit").addEventListener("click", function (event) {
     event.preventDefault()
-    var nombre = document.getElementById("nombre");
+    var nombre = document.getElementById("usuario");
     var password = document.getElementById("password");
 
     if (!checkInput(nombre) || !checkInput(password)) {
-        document.getElementById("errorMsg").innerHTML = "Corrija los errores";
+        document.getElementById("errores").innerHTML = "Corrija los errores";
     } else {
         fetch('http://localhost:3000/usuarios')
             .then(response => response.json())
             .then(data => {
                 [...data].some(function (element) {
                     if (element["nombre"] == nombre.value && element["password"] == password.value) {
-
-                        // No funciona en Chrome
-                        // Funciona en Firefox
-                        document.cookie = `id=${element["id"]}`;
                         document.cookie = `username=${element["nombre"]}`;
                         document.cookie = `password=${element["password"]}`;
-                        location.href="crud.html";
+                        location.href = "index.html";
                         return true;
                     } else {
-                        document.getElementById("errorMsg").innerHTML = "El nombre de usuario o la contraseña son incorrectos";
+                        document.getElementById("errores").innerHTML = "El nombre de usuario o la contraseña son incorrectos";
                     }
                 })
             })
             .catch(error => console.error('Error al obtener usuarios:', error));
-        document.getElementById("errorMsg").innerHTML = "";
+        document.getElementById("errores").innerHTML = "";
     }
 });
-
